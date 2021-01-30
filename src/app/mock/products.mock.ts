4a47c6c77d8ccf41fe90f16f37e5f2d64d33e2d6
David Morales <dsmorales349@misena.edu.co>
2021-01-30 15:02:34 -0500

    add models and mock data

---
+++ src/app/mock/products.mock.ts
@@ -0,0 +1,147 @@
+export interface Product {
+  photoUrl: string;
+  name: string;
+  qty: number;
+  measurementUnit: string;
+  price: number;
+  careType: string;
+  brand: string;
+  applicationTime: string;
+}
+
+const products: Product[] = [
+  {
+    photoUrl:
+      'https://i.picsum.photos/id/939/200/200.jpg?hmac=iqiSg4LCwK5ANmtBOgb0nQOLsoedkjQNxc_AH-WNlFc',
+    name: 'Esto es un nombre',
+    qty: 25,
+    measurementUnit: 'ml',
+    price: 30000,
+    careType: 'bodily',
+    brand: 'Pepito',
+    applicationTime: 'day',
+  },
+  {
+    photoUrl:
+      'https://i.picsum.photos/id/939/200/200.jpg?hmac=iqiSg4LCwK5ANmtBOgb0nQOLsoedkjQNxc_AH-WNlFc',
+    name: 'Esto es un nombre',
+    qty: 25,
+    measurementUnit: 'ml',
+    price: 30000,
+    careType: 'bodily',
+    brand: 'Pepito',
+    applicationTime: 'day',
+  },
+  {
+    photoUrl:
+      'https://i.picsum.photos/id/939/200/200.jpg?hmac=iqiSg4LCwK5ANmtBOgb0nQOLsoedkjQNxc_AH-WNlFc',
+    name: 'Esto es un nombre',
+    qty: 25,
+    measurementUnit: 'ml',
+    price: 30000,
+    careType: 'bodily',
+    brand: 'Pepito',
+    applicationTime: 'day',
+  },
+  {
+    photoUrl:
+      'https://i.picsum.photos/id/939/200/200.jpg?hmac=iqiSg4LCwK5ANmtBOgb0nQOLsoedkjQNxc_AH-WNlFc',
+    name: 'Esto es un nombre',
+    qty: 25,
+    measurementUnit: 'ml',
+    price: 30000,
+    careType: 'bodily',
+    brand: 'Pepito',
+    applicationTime: 'day',
+  },
+  {
+    photoUrl:
+      'https://i.picsum.photos/id/939/200/200.jpg?hmac=iqiSg4LCwK5ANmtBOgb0nQOLsoedkjQNxc_AH-WNlFc',
+    name: 'Esto es un nombre',
+    qty: 25,
+    measurementUnit: 'ml',
+    price: 30000,
+    careType: 'bodily',
+    brand: 'Pepito',
+    applicationTime: 'day',
+  },
+  {
+    photoUrl:
+      'https://i.picsum.photos/id/939/200/200.jpg?hmac=iqiSg4LCwK5ANmtBOgb0nQOLsoedkjQNxc_AH-WNlFc',
+    name: 'Esto es un nombre',
+    qty: 25,
+    measurementUnit: 'ml',
+    price: 30000,
+    careType: 'bodily',
+    brand: 'Pepito',
+    applicationTime: 'day',
+  },
+  {
+    photoUrl:
+      'https://i.picsum.photos/id/939/200/200.jpg?hmac=iqiSg4LCwK5ANmtBOgb0nQOLsoedkjQNxc_AH-WNlFc',
+    name: 'Esto es un nombre',
+    qty: 25,
+    measurementUnit: 'ml',
+    price: 30000,
+    careType: 'bodily',
+    brand: 'Pepito',
+    applicationTime: 'day',
+  },
+  {
+    photoUrl:
+      'https://i.picsum.photos/id/939/200/200.jpg?hmac=iqiSg4LCwK5ANmtBOgb0nQOLsoedkjQNxc_AH-WNlFc',
+    name: 'Esto es un nombre',
+    qty: 25,
+    measurementUnit: 'ml',
+    price: 30000,
+    careType: 'bodily',
+    brand: 'Pepito',
+    applicationTime: 'day',
+  },
+  {
+    photoUrl:
+      'https://i.picsum.photos/id/939/200/200.jpg?hmac=iqiSg4LCwK5ANmtBOgb0nQOLsoedkjQNxc_AH-WNlFc',
+    name: 'Esto es un nombre',
+    qty: 25,
+    measurementUnit: 'ml',
+    price: 30000,
+    careType: 'bodily',
+    brand: 'Pepito',
+    applicationTime: 'day',
+  },
+  {
+    photoUrl:
+      'https://i.picsum.photos/id/939/200/200.jpg?hmac=iqiSg4LCwK5ANmtBOgb0nQOLsoedkjQNxc_AH-WNlFc',
+    name: 'Esto es un nombre',
+    qty: 25,
+    measurementUnit: 'ml',
+    price: 30000,
+    careType: 'bodily',
+    brand: 'Pepito',
+    applicationTime: 'day',
+  },
+  {
+    photoUrl:
+      'https://i.picsum.photos/id/939/200/200.jpg?hmac=iqiSg4LCwK5ANmtBOgb0nQOLsoedkjQNxc_AH-WNlFc',
+    name: 'Esto es un nombre',
+    qty: 25,
+    measurementUnit: 'ml',
+    price: 30000,
+    careType: 'bodily',
+    brand: 'Pepito',
+    applicationTime: 'day',
+  },
+  {
+    photoUrl:
+      'https://i.picsum.photos/id/939/200/200.jpg?hmac=iqiSg4LCwK5ANmtBOgb0nQOLsoedkjQNxc_AH-WNlFc',
+    name: 'Esto es un nombre',
+    qty: 25,
+    measurementUnit: 'ml',
+    price: 30000,
+    careType: 'bodily',
+    brand: 'Pepito',
+    applicationTime: 'day',
+  },
+];
+
+export default products;
